f27768178c4568ae9f263d26e8eb87958f83e688
Nicholas McDonald <nmcdonaldfhs@gmail.com>
2020-12-15 23:54:39 -0800

    Initial Commit

---
+++ Alectrona/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Alectrona
+//
+//  Created by Nicholas McDonald on 12/15/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
